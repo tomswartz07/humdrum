40b4b058179c54d063cd551791383f934bc042e0
Tom Swartz <tom@pennmanor.net>
2014-02-03 18:07:25 -0500

    Initial Commit
    Initial implementation of the extension. Currently only replaces several
    short phrases and words.

---
+++ README.md
@@ -0,0 +1,10 @@
+cloud-to-butt
+=============
+
+Chrome extension that replaces occurrences of over-hyped text with reasonable and demure text.
+
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions. Select 'Load Unpacked Extension' and select the source code.
---
+++ content_script.js
@@ -0,0 +1,42 @@
+walk(document.body);
+
+// I've borrowed this script from 'Cloud-to-Butt' Google Chrome
+// Extension. Sourced on GitHub.
+
+function walk(node)
+{
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+// TODO Figure out how to insert a dictionary style find/replace
+	v = v.replace(/\bliterally\b/g, "figuratively");
+	v = v.replace(/\bLiterally\b/g, "Figuratively");
+	v = v.replace(/\bamazing\b/g, "ordinary");
+	v = v.replace(/\bAmazing\b/g, "Ordinary");
+
+	textNode.nodeValue = v;
+}
+
+
