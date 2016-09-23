8c22e4b962f4e5b44cccd60d3dde049ad77cb36d
Tom Swartz <tom.swartz07@gmail.com>
2016-09-23 11:08:14 -0400

    Millenials = Lizard People

--- content_script.js
+++ content_script.js
@@ -31,6 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 // TODO Figure out how to insert a dictionary style find/replace
+	v = v.replace(/\bmillenials\b/g, "Lizard People");
+	v = v.replace(/\bpoliticians\b/g, "Lizard People");
+	v = v.replace(/\bpolitician\b/g, "Lizard Person");
 	v = v.replace(/\bliterally\b/g, "figuratively");
 	v = v.replace(/\bLiterally\b/g, "Figuratively");
 	v = v.replace(/\bamazing\b/g, "ordinary");
